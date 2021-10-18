20e6d7ff8d323654d7fc0fa780425c0b53dc1ba0
Abhishek Pal <42897033+noob77777@users.noreply.github.com>
2021-10-18 18:38:18 +0530

    :sparkles: add text and logos (#3)

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -4,13 +4,26 @@ const About = (props: any) => {
       <h2 className="section__title">About Us</h2>
       <span className="section__subtitle">What we do</span>
       <div className="about__container container grid">
-        <img src="img/about.png" alt="" className="about__img" />
+        <img src="img/arch.svg" alt="" height="400" className="about__img" />
         <div className="about__data">
           <p className="about__description">{props.about.text}</p>
           <div className="about__info">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+              <span className="about__info-title">
+                {props.about.info[0].title}
+              </span>
+              <span className="about__info-name">
+                {props.about.info[0].name}
+              </span>
+            </div>
+            <div>
+              <span className="about__info-title">
+                {props.about.info[1].title}
+              </span>
+              <span className="about__info-name">
+                {props.about.info[1].name}
+              </span>
+            </div>
           </div>
           <div className="about__buttons"></div>
         </div>
